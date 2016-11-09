53ad359e6393a5014ccbd4840394be460e4094ec
estermer <eric.stermer@gmail.com>
2016-11-09 15:59:18 -0500

    created a custom directive element with the create-poll html

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,13 @@
 (function(){
   var app = angular.module('pollster', []);
 
+  app.directive('createPoll', function(){
+    return {
+      restrict: 'E',
+      templateUrl: '/templates/create-poll.html'
+    };
+  });
+
   app.controller('MainCtrl', function(){
 
 
