400a8ea26a8afcec987f3eb6860a5091ed4a7160
estermer <eric.stermer@gmail.com>
2016-11-09 22:34:25 -0500

    added http get to get the polls and a post to create one and return all the polls

--- public/js/app.js
+++ public/js/app.js
@@ -8,8 +8,33 @@
     };
   });
 
-  app.controller('MainCtrl', function(){
+  app.directive('createAnswer', function(){
+    return {
+      restrict: 'E',
+      templateUrl: '/templates/create-poll.html'
+    };
+  });
 
+  app.controller('MainCtrl', function($http){
+    var rootUrl = 'http://localhost:3000/';
+    var self = this;
+
+    self.pollCreated = false;
+
+    $http.get(`${rootUrl}/polls`)
+      .then(function(response){
+        console.log(response.data.status);
+        self.polls = response.data.polls;
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+
+
+
+    self.createPoll = function(poll){
+      $http.post('/polls')
+    }
 
 
   });
