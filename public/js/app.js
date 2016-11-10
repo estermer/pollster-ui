e180365d71c92d81ac576994af5b25844a398f21
estermer <eric.stermer@gmail.com>
2016-11-09 23:29:47 -0500

    Added a function that ctakes to answers object and turns the values into an array of answers
    created a function to get all the polls and called it upon window load

--- public/js/app.js
+++ public/js/app.js
@@ -16,26 +16,68 @@
   });
 
   app.controller('MainCtrl', function($http){
-    var rootUrl = 'http://localhost:3000/';
+    var rootUrl = 'http://localhost:3000';
     var self = this;
 
     self.pollCreated = false;
 
-    $http.get(`${rootUrl}/polls`)
-      .then(function(response){
-        console.log(response.data.status);
-        self.polls = response.data.polls;
-      })
-      .catch(function(err){
-        console.log(err);
-      });
-
-
+    var getAllPolls = function(){
+      $http.get(`${rootUrl}/polls`)
+        .then(function(response){
+          console.log(response.data.status);
+          self.polls = response.data.polls;
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+    };
 
     self.createPoll = function(poll){
-      $http.post('/polls')
-    }
+      $http.post('/polls', poll)
+        .then(function(response){
+          //make pollCreated true to then
+          //show the create answers directive element
+          self.pollCreated = true;
+          self.newPoll = response.data.poll;
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+    };
 
+    self.createAnswers = function(answers){
+      //first change the answers to an array of answers
+      answers = changeAnswersToArray(answers);
+
+      //then send them to the api as the array
+      $http.post(`${rootUrl}/polls/${self.newPoll.id}/answers`, {answers: answers})
+        .then(function(response){
+          self.pollCreated = false;
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+    };
+
+    //this function turns the answers object into
+    //and array of answers to send to the backend
+    function changeAnswersToArray(answers){
+      var array = [];
+
+      if (answers == {} || answers == null){
+        return "no answers to create";
+      }
+
+      for(var x in answers){
+        if(answers.x != null || answers.x != ""){
+          array.push(answers.x);
+        }
+      }
+      return array;
+    };
+
+    //get all polls created upon window load
+    getAllPolls();
 
   });
 
